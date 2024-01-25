6edea52cf9914d7b51f600f67382e50aadd9b540
stix2you <stix2you@gmail.com>
2024-01-25 15:06:49 +1300

    update grade function added

--- test.js
+++ test.js
@@ -56,5 +56,27 @@ app.post('/students', (req, res) => {
     }
 });
 
+// Deletes a student from our list, by ID
+app.delete('/students/:id', (req, res) => {
+    let student = students.find((student) => { return student.id === req.params.id});
+
+    if (student) {
+        students = students.filter((obj) => { return obj.id !== req.params.id });
+        res.status(201).send('Student ' + req.params.id + ' was deleted.');
+    }
+});
+
+// Updates the "grade" of a student by student name/class name
+app.put('/students/:name/:class/:grade', (req, res) => {
+    let student = students.find((student) => { return student.name === req.params.name });
+   
+    if (student) {
+        student.classes[req.params.class] = parseInt(req.params.grade);
+        res.status(201).send('Student ' + req.params.name + ' was assigned a grade of ' + req.params.grade + ' in ' + req.params.class + '.');
+    } else {
+        res.status(404).send('Student with name ' + req.params.name + ' was not found.');
+    }
+});
+
 
 
